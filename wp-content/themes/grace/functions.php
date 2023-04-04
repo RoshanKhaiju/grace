4763d33e0b820e494bc019da5a9a5f6b8ffa7590
RoshanKhaiju <khaijur5@gmail.com>
2023-04-04 12:25:29 +0545

    script enqueue issue solved

--- wp-content/themes/grace/functions.php
+++ wp-content/themes/grace/functions.php
@@ -29,17 +29,17 @@ add_action('init', 'grace_theme_support');
 function grace_theme_scripts()
 {
     // enqueue styles
-    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.css', [], false, 'all');
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.css');
 
-    wp_enqueue_style('grace-css', get_template_directory_uri() . '/assets/css/style.css', [], false, 'all');
+    wp_enqueue_style('grace-css', get_template_directory_uri() . '/assets/css/style.css');
 
-    wp_enqueue_style('dark-css', get_template_directory_uri() . '/assets/css/dark.css', [], false, 'all');
+    wp_enqueue_style('dark-css', get_template_directory_uri() . '/assets/css/dark.css');
 
-    wp_enqueue_style('font-icons', get_template_directory_uri() . '/assets/css/font-icons.css', [], false, 'all');
+    wp_enqueue_style('font-icons', get_template_directory_uri() . '/assets/css/font-icons.css');
 
-    wp_enqueue_style('animate-css', get_template_directory_uri() . '/assets/css/animate.css', [], false, 'all');
+    wp_enqueue_style('animate-css', get_template_directory_uri() . '/assets/css/animate.css');
 
-    wp_enqueue_style('mangnific-css', get_template_directory_uri() . '/assets/css/magnific-popup.css', [], false, 'all');
+    wp_enqueue_style('mangnific-css', get_template_directory_uri() . '/assets/css/magnific-popup.css');
 
 
     /*
@@ -47,77 +47,77 @@ function grace_theme_scripts()
     sliders revolution 5.x css settings
     ========================================
     */
-    wp_enqueue_style('setting-css', get_template_directory_uri() . '/assets/rs-plugin/css/settings.css', [], false, 'all');
+    wp_enqueue_style('setting-css', get_template_directory_uri() . '/assets/rs-plugin/css/settings.css');
 
-    wp_enqueue_style('layers-css', get_template_directory_uri() . '/assets/rs-plugin/css/layers.css', [], false, 'all');
+    wp_enqueue_style('layers-css', get_template_directory_uri() . '/assets/rs-plugin/css/layers.css');
 
-    wp_enqueue_style('navigation-css', get_template_directory_uri() . '/assets/rs-plugin/css/navigation.css', [], false, 'all');
+    wp_enqueue_style('navigation-css', get_template_directory_uri() . '/assets/rs-plugin/css/navigation.css');
 
     /*
     ========================================
     recipes demo specific stylesheet
     ========================================
     */
-    wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/demos/recipes/css/fonts.css', [], false, 'all');
+    wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/demos/recipes/css/fonts.css');
 
     /*
     ========================================
     Theme Font
     ========================================
     */
-    wp_enqueue_style('recipes-css', get_template_directory_uri() . '/assets/demos/recipes/recipes.css', [], false, 'all');
+    wp_enqueue_style('recipes-css', get_template_directory_uri() . '/assets/demos/recipes/recipes.css');
 
     /*
     ========================================
     custom stylesheet
     ========================================
     */
-    wp_enqueue_style('custom-css', get_template_directory_uri() . '/assets/css/custom.css', [], false, 'all');
+    wp_enqueue_style('custom-css', get_template_directory_uri() . '/assets/css/custom.css');
 
     wp_enqueue_style('style-css', get_stylesheet_uri());
 
 
 
     // enqueue scripts
-    wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.js', [], false, true);
-    wp_enqueue_script('plugin', get_template_directory_uri() . '/assets/js/plugins.min.js', ['jquery'], false, false);
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.js');
+    wp_enqueue_script('plugin', get_template_directory_uri() . '/assets/js/plugins.min.js');
 
     /*
     ========================================
     slider revolution 5.x scripts
     ========================================
     */
-    wp_enqueue_script('tools-js', get_template_directory_uri() . '/assets/rs-plugin/js/jquery.themepunch.tools.min.js', [], false, true);
-    wp_enqueue_script('revolution-js', get_template_directory_uri() . '/assets/rs-plugin/js/jquery.themepunch.revolution.min.js', [], false, true);
+    wp_enqueue_script('tools-js', get_template_directory_uri() . '/assets/rs-plugin/js/jquery.themepunch.tools.min.js');
+    wp_enqueue_script('revolution-js', get_template_directory_uri() . '/assets/rs-plugin/js/jquery.themepunch.revolution.min.js');
 
     /*
     ========================================
     add-ons js
     ========================================
     */
-    wp_enqueue_script('pixi-js', get_template_directory_uri() . '/assets/rs-plugin/js/pixi.min.js', [], false, true);
+    wp_enqueue_script('pixi-js', get_template_directory_uri() . '/assets/rs-plugin/js/pixi.min.js');
 
     /*
     ========================================
     slider revolution extensions
     ========================================
     */
-    wp_enqueue_script('action-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.actions.min.js', [], false, true);
-    wp_enqueue_script('carousel-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.carousel.min.js', [], false, true);
-    wp_enqueue_script('kenburn-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.kenburn.min.js', [], false, true);
-    wp_enqueue_script('layeranimation-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js', [], false, true);
-    wp_enqueue_script('migration-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.migration.min.js', [], false, true);
-    wp_enqueue_script('navigation-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.navigation.min.js', [], false, true);
-    wp_enqueue_script('parallax-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.parallax.min.js', [], false, true);
-    wp_enqueue_script('slideanims-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.slideanims.min.js', [], false, true);
-    wp_enqueue_script('video-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.video.min.js', [], false, true);
+    wp_enqueue_script('action-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.actions.min.js');
+    wp_enqueue_script('carousel-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.carousel.min.js');
+    wp_enqueue_script('kenburn-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.kenburn.min.js');
+    wp_enqueue_script('layeranimation-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js');
+    wp_enqueue_script('migration-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.migration.min.js');
+    wp_enqueue_script('navigation-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.navigation.min.js');
+    wp_enqueue_script('parallax-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.parallax.min.js');
+    wp_enqueue_script('slideanims-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.slideanims.min.js');
+    wp_enqueue_script('video-js', get_template_directory_uri() . '/assets/rs-plugin/js/extensions/revolution.extension.video.min.js');
 
     /*
     ========================================
     footer scripts
     ========================================
     */
-    wp_enqueue_script('functions-js', get_template_directory_uri() . '/assets/js/functions.js', [], false, true);
+    wp_enqueue_script('functions-js', get_template_directory_uri() . '/assets/js/functions.js');
 }
 add_action('wp_enqueue_scripts', 'grace_theme_scripts');
 
